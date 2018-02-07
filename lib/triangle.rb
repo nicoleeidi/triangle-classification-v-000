3af3ecdecbc23d9bc595f91f31f063a9d5ca8868
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-07 01:48:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,18 @@ class Triangle
   def kind
     if @length == @width && @length == @height
       return
+
+      if @length + @width <= @height || @width + @height <= @length || @length + @height <= @width 
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
   # write code here
 end
 class TriangleError < StandardError
-end 
+end
